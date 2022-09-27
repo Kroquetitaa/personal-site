9a9278297bf675932a46dfcdc20dc587762a69d3
Kroquetitaa <Kroquetita@gmail.com>
2022-09-28 00:22:13 +0200

    add skills

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,149 @@
+import React from "react";
+import "./Skills.css";
+
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h2>Skills</h2>
+      <div className="skills-data">
+        <div className="skill">
+          <h3 className="skill-title">HTML</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">CSS</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">SCSS</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">BootStrap</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">MUI</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">Chakra UI</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">Tailwind</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">IONIC</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">JavaScript</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">TypeScript</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">React</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">Angular</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">Next.JS</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">Node</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-medium"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+        <div className="skill">
+          <h3 className="skill-title">MongoDB</h3>
+          <span className="skill-dot"></span>
+          <span className="skill-dot"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+          <span className="skill-dot-white"></span>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
