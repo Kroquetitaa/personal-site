aea6e71bcf405c55dcb4366e37c818065553e937
Kroquetitaa <Kroquetita@gmail.com>
2022-09-28 00:44:36 +0200

    add experiency

--- src/components/Experiency/Experiency.jsx
+++ src/components/Experiency/Experiency.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React from "react";
+import "./Experiency.css";
 
 const Experiency = () => {
   return (
-    <div>Experiency</div>
-  )
-}
+    <div className="experiency">
+      <div className="experiency--card">
+        <h3>FullStack Developer Junior</h3>
+        <p>
+          <strong>Company:</strong> Emiral | January 2022 - July 2022
+        </p>
+        <p>
+          - Backend programmer using scripting for Sage (ERP), along with
+          node.JS in ERP linking. Along with testings and testing testing of
+          mailings to Sengrid's (Bulk Mail) platform.
+        </p>
+        <p>
+          - Frontend developer with Angular and the IONIC framework for
+          building, testing, development and debbugging of WebApps and APIS
+          consumption, along with its creation with .NET . Handling for
+          development as production
+        </p>
 
-export default Experiency
+        <p>- Azure DevOps Service Management </p>
+        <p>
+          - Use of Docker and PostGreSQL together with MONGODB, in their default
+          use bbdd creation for WEBAPPS. Interconnection with frontend and
+          backend.
+        </p>
+      </div>
+      <div className="experiency--card">
+        <h3>Frontend Developer Junior</h3>
+        <p>
+          <strong>Company:</strong> DedeIT | June 2021 - January 2022
+        </p>
+        <p>
+          - React developer for Web infrastructure, small TDD developments with
+          TDD developments with JEST
+        </p>
+        <p>
+          - Node developer for distributed systems and communications: queues
+          communications: queues, events. . .
+        </p>
+
+        <p>- Docker Management</p>
+        <p>
+          -  UX/UI for applications.
+        </p>
+        <p>- WPO Web Performance Optimization</p>
+      </div>
+    </div>
+  );
+};
+
+export default Experiency;
